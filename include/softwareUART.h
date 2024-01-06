9130f0aeb7c9c3ee2e90f009d74193b30a04ca9f
Ethan <ethanhharris5@gmail.com>
2024-01-06 22:31:05 +1100

    Added UART config, transmit and parity calculation functions

--- include/softwareUART.h
+++ include/softwareUART.h
@@ -1,13 +1,30 @@
 #include <stdint.h>
+#include "esp32-hal-gpio.h"
+
+#define millisecondsInSecond 1000000
+#define bitTime(baudRate) millisecondsInSecond / baudRate
 
 typedef struct {
-    uint16_t start_bit  : 1;
+    //uint16_t start_bit  : 1;
     uint16_t payload    : 8;
     uint16_t parity_bit : 1; 
-    uint16_t stop_bit   : 2;
+    //uint16_t stop_bit   : 2;
 } UARTPacket;
 
 
+// tx and rx are oriented to this device.
+// I.E tx_pin is the pin that THIS device will send data through
 typedef struct {
-
+    uint32_t baud_rate;
+    uint8_t tx_pin;
+    uint8_t rx_pin; 
 } UARTConfig;
+
+
+typedef enum {
+    GENERIC_ERROR = 0,
+    SUCCESS = 1
+} UARTStatusType;
+
+
+UARTStatusType SUART_init(UARTConfig *conf);
--- src/softwareUART.c
+++ src/softwareUART.c
@@ -1,2 +1,49 @@
 #include "softwareUART.h"
 
+void _calculate_parity(UARTPacket *pkt) {
+    uint8_t tmp_payload = pkt->payload;
+
+    // Loop through payload and flip parity for each '1' present to ensure an even payload+parity
+    for(uint8_t idx = 0; idx < 8; idx++) {
+        if(tmp_payload & 0x01) {
+            pkt->parity_bit = !pkt->parity_bit;
+        }
+        tmp_payload = tmp_payload >> 1;
+    }
+}
+
+UARTStatusType SUART_init(UARTConfig *conf) {
+    pinMode(conf->rx_pin, INPUT);
+    pinMode(conf->tx_pin, OUTPUT);
+
+    digitalWrite(conf->tx_pin, HIGH); // TX Line idles high
+
+    return SUCCESS;
+}
+
+UARTStatusType UART_transmit(UARTConfig *conf, UARTPacket *pkt) {
+    // Init as much as we can before gpio actions
+    uint8_t tmp_payload = pkt;
+    _calculate_parity(pkt);
+    
+    // Start bit
+    digitalWrite(conf->tx_pin, LOW);
+    delayMicroseconds( bitTime(conf->baud_rate) );
+
+    // Write payload
+    for(uint8_t idx = 0; idx < 8; idx++) {
+        digitalWrite(conf->tx_pin, tmp_payload & 0x01);
+        tmp_payload = tmp_payload >> 1;
+        delayMicroseconds( bitTime(conf->baud_rate) );
+    }
+
+    // Write parity bit
+    digitalWrite(conf->tx_pin, pkt->parity_bit);
+    delayMicroseconds( bitTime(conf->baud_rate) );
+
+    // Stop bit/s
+    digitalWrite(conf->tx_pin, HIGH);
+    delayMicroseconds( 2 * bitTime(conf->baud_rate) );
+
+    return SUCCESS;
+}
